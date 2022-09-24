f1d149fed514e97cb420c7454196951a48735c47
Colton Hurst <colton@coltonhurst.com>
2022-09-24 00:47:35 -0400

    Initial commit with basic functionality for TidyTube

---
+++ README.md
@@ -0,0 +1,7 @@
+# TidyTube
+
+A Firefox browser extension that cleans up YouTube and gives you the info you need.
+
+### Attributions
+
+- [Cleaning icons created by Sir.Vector - Flaticon](https://www.flaticon.com/free-icons/cleaning)
---
+++ tidytube.js
@@ -0,0 +1,34 @@
+// ---------------------
+// ----- Tidy Tube -----
+// ---------------------
+
+window.addEventListener("load", main, false);
+const MAX_SIDEBAR_REMOVAL_RETRY_INTERVAL_MS = 100;
+const MAX_SIDEBAR_REMOVAL_RETRIES_ALLOWED = 10;
+
+function main() {
+    let removeSidebarTimer = setInterval(removeSidebar, MAX_SIDEBAR_REMOVAL_RETRY_INTERVAL_MS);
+    let sidebarRemovalAttempts = 0;
+
+    function removeSidebar() {
+        if (sidebarRemovalAttempts >= MAX_SIDEBAR_REMOVAL_RETRIES_ALLOWED) {
+            // give up
+            clearInterval(removeSidebarTimer);
+            return;
+        } else {
+            sidebarRemovalAttempts++;
+        }
+
+        try {
+            console.log("in the try");
+            const sidebar = document.getElementById("secondary");
+
+            if (sidebar != undefined && sidebar != null) {
+                clearInterval(removeSidebarTimer);
+                sidebar.remove();
+            }
+        } catch (error) {
+            console.log(error);
+        }
+    }
+}
